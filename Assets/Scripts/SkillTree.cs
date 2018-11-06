18f712b2e66067775aee2abbd3c15648fdb6a1f0
Carelle <steamtik@gmail.com>
2018-11-06 09:49:57 -0800

    init

--- Assets/Scripts/SkillTree.cs
+++ Assets/Scripts/SkillTree.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 /*TODO
-place SkillTree into here instead
 Make a seperate script to handle UI
 
 In the Script that handles the UI make it pop open a window that shows the damage and requirements 
@@ -12,8 +11,8 @@ get this to work with overing rather then clicking
 [System.Serializable]
 public class Skills
 {
-
     public string skillName;
+    public ParticleEmitter SkillEffects;
     public float damage;
     public Button skillButton;
     public KeyCode key;
