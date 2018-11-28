f745b8fa10907bc5d8faed9286b5f9123eab0e76
Carelle <steamtik@gmail.com>
2018-11-28 12:44:55 -0800

    Finished Cooldowns

--- Assets/Scripts/SkillTree.cs
+++ Assets/Scripts/SkillTree.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 /*TODO
-        Add in cool downs
         Make a PlayerInfo script
+        Make a UI Script
+        Make so that way the Choosen Skill bool is more Auto
         replace LevelingShit with PlayerInfo
         Make the Inspector UI look better end easier to navagate
         TEST
@@ -20,7 +21,7 @@ public class Skills
     public float damage;    
     public KeyCode[] combo;
     [Header("_|Bool|___")]
-    public bool unlock;
+    public bool unlock = false;
     public bool choosenSkill = false;
     [Header("_|Level Requirements|___")]
     public Button[] requiredSkill;
@@ -32,9 +33,9 @@ public class Skills
     public Text skillText;
     public Text LevelReqirement;
     public Text Damage;
-    //[HideInInspector]
+    public Text cooldown;
+    [HideInInspector]
     public float cooldownHandle;
-
 }
 
 public class SkillTree : MonoBehaviour
@@ -56,9 +57,7 @@ public class SkillTree : MonoBehaviour
             skills[i].skillText.text = skills[i].skillName; //Sets text to teh skill name
             skills[i].SkillEffects.gameObject.SetActive(false);
             skills[i].skillButton.interactable = false;
-            skills[i].unlock = false;
             skills[i].cooldownHandle = skills[i].coolDown;
-
         }
     }
     // Update is called once per frame
@@ -89,7 +88,7 @@ public class SkillTree : MonoBehaviour
                     {
                         //Debug.Log("Timer started!");
                         isTimerRunning = true;
-                        StartCoroutine(KeyReader());
+                        StartCoroutine(KeyReader(/*i*/));
                         break;
                     }
                 }
@@ -98,11 +97,8 @@ public class SkillTree : MonoBehaviour
 
         for (int i = 0; i < skills.Count; i++)
         {
-
             if (leveling.levelNumber >= skills[i].level && skills[i].requiredSkill.Length == 0) //checks to see if the player is of the correct level
             {
-                int levelinfo = skills[i].level - 1;
-
                 skills[i].skillButton.interactable = true; //make button interactiable if level requarment is met
             }
             else if (skills[i].unlock == true && leveling.levelNumber >= skills[i].level - 1)
@@ -114,12 +110,10 @@ public class SkillTree : MonoBehaviour
                 skills[i].skillButton.interactable = false;
             }
         }
-
         if (isTimerRunning == false)//This does not belong in the for loop, doing so causes it to clear out the storedKeys way to early
         {
             storedKeys.Clear();
         }
-
     }
     public void BoolCheck(string skillName)
     {
@@ -141,40 +135,36 @@ public class SkillTree : MonoBehaviour
             storedKeys.Add(e.keyCode);
         }
     }
-    IEnumerator KeyReader()
+    IEnumerator KeyReader(/*int i*/)
     {
         yield return new WaitForSeconds(1);
 
         for (int i = 0; i < skills.Count; i++)
         {
-            if (storedKeys.Count != skills[i].combo.Length) { continue; }
+            if (storedKeys.Count != skills[i].combo.Length)
+            { continue; }
 
-            bool match = true;
-            for (int j = 0; j < skills[i].combo.Length; j++)
+                bool match = true;
+        for (int j = 0; j < skills[i].combo.Length; j++)
+        {
+            if (storedKeys[j] != skills[i].combo[j])
             {
-                if (storedKeys[j] != skills[i].combo[j])
-                {
-                    match = false;
-                    break;
-                }
+                match = false;
+                break;
             }
-            if (match && skills[i].manaCost <= leveling.manaAmount && skills[i].cooldownHandle > 0)
+        }
+        if (match && skills[i].manaCost <= leveling.manaAmount && skills[i].cooldownHandle >= skills[i].coolDown)
+        {
+            StartCoroutine(CoolDown(i));
+            leveling.manaAmount -= skills[i].manaCost;
+            if (!skills[i].SkillEffects.gameObject.activeInHierarchy)
             {
-                StartCoroutine(CoolDown(skills[i].cooldownHandle, skills[i].coolDown));
- 
-
-                leveling.manaAmount -= skills[i].manaCost;
-                leveling.mana.text = "Mana: " + leveling.manaAmount;
-
-                Debug.Log(skills[i].skillName);
-                if (!skills[i].SkillEffects.gameObject.activeInHierarchy)
-                {
-                    skills[i].SkillEffects.gameObject.SetActive(true);
-                }
-                else
-                {
-                    skills[i].SkillEffects.Play();
-                }
+                skills[i].SkillEffects.gameObject.SetActive(true);
+            }
+            else
+            {
+                skills[i].SkillEffects.Play();
+            }
                 break;
             }
         }
@@ -182,19 +172,17 @@ public class SkillTree : MonoBehaviour
         //Debug.Log("Timer stopped!");
         isTimerRunning = false;
     }
-    IEnumerator CoolDown(float cooldownHandle, float cooldown)
+    IEnumerator CoolDown(int i)
     {
-        while (cooldownHandle > 0)
+        while(skills[i].cooldownHandle > 0)
         {
-            
-            cooldownHandle -= Time.deltaTime;
+            skills[i].cooldownHandle -= 1;
+            skills[i].cooldown.text = "Cool Down: " + skills[i].cooldownHandle;
             yield return new WaitForSeconds(1);
+           
         }
-        if (cooldown <= 0)
-        {
-            cooldownHandle = cooldown;
-        }
+        skills[i].cooldownHandle = skills[i].coolDown;
+       
     }
-
 }
 
